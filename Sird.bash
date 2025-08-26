2aec646ebb6cd71c118fb804b0205791a30008e4
SindiKali <m4ttpizz4@gmail.com>
2025-08-26 12:32:47 +0000

    origin/main Código do arquivo Sird.bash

---
+++ Sird.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+real -p "Digite a URL do site: " url
+files = (
+    ".env"
+    "backup.sql"
+    "config.php"
+    "admin/"
+    ".pdf"
+)
+
+for file in "${files[@]}"
+do
+    response=$(curl -s -o /dev/null -w "%{http_code}" "$url/$file")
+
+    if [ "$response" == "200" ]
+    then
+        echo "Arquivo encontrado: $url/$file"
+    fi
+done
